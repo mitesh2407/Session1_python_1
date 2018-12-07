5734e478272e5a453a7b75e8cc338246115638ed
mitesh2407 <mitesh2407@gmail.com>
2018-12-06 23:52:00 -0800

    Add files via upload

--- Python1_Session_assignment_1.py
+++ Python1_Session_assignment_1.py
@@ -4,37 +4,41 @@ Created on Fri Dec  7 11:55:08 2018
 
 @author: acer
 """
-# 
+# #############################################################################
 #       PYTHON1_SESSION1_ASSIGNMENT_1 
 #   Date   :07-12-2018
 #   Author : Mitesh Chandra    
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #  
+#############################################################################
 # 1.Write a program which will find all such numbers which are divisible by 7 
 #   but are not a multiple of 5, between 2000 and 3200 (both included). The 
 #   numbers obtained should be printed in a comma-separated sequence on a 
 #   single line.
+#############################################################################
+
 list =[x for x in range (2000,3201) if x % 7 == 0 and x % 5 != 0];
 print (list)
 
 
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #  
+############################################################################# 
 # 3. Write a Python program to accept the user's first and last name and then 
 #    getting them printed in the the reverse order with a space between first 
 #    name and last name.
+#############################################################################
 
 fname = input("Input your First Name : ");
 lname = input("Input your Last Name : ");
 print ("Hello  " + lname + " " + fname)
 
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#############################################################################
 # 4. Write a Python program to find the volume of a sphere with diameter 12 cm.
 #    Formula: V=4/3 * π * r
+#############################################################################
 
 import math;
 pi = math.pi;
-#Converting Diameter into radius
+# Converting Diameter into radius
 r = float(12/2);
 V = (float(4/3)*(pi)*(r)**3);
 print(V)
